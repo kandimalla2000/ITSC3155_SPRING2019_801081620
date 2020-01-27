d171043e2df084891462e25ed1863b5694e72c4f
Saisiddharth Kandimalla <skandim1@uncc.edu>
2020-01-27 03:41:27 +0000

    RubyHashes - Part 2

--- Labs/RubyHashes/lib/hashes.rb
+++ Labs/RubyHashes/lib/hashes.rb
@@ -1,15 +1,48 @@
 # RubyHashes
 # Part I
 def array_2_hash emails, contacts
-    # YOUR CODE HERE
+    if emails.length() == 0
+        return contacts
+    else
+        i = 0
+        contacts.each do |k,v|
+            contacts[k] = emails[i]
+            i = i+ 1
+        end
+    end
+    return contacts
 end
 
 # Part II
 def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
+    i = 0
+    base = Hash.new
+    if contact_info.length() == 0 || contact_info[0].length() == 0
+        contacts.each do |k,v|
+            base[k.to_sym] = {}
+            i = i+ 1
+        end
+    else
+        contacts.each do |k,v|
+            base[k.to_sym] = {:email=>contact_info[i][0], :phone=>contact_info[i][1]}
+            i = i+ 1
+        end
+        
+    end
+    return base
 end
 
 # Part III
 def hash_2_array contacts
-    # YOUR CODE HERE
+    arrName = []
+    arrEmails= []
+    arrPN = []
+    i = 0
+    contacts.each do |k,v|
+        arrName.push(k.to_s)
+        arrEmails.push(v[:email])
+        arrPN.push(v[:phone])
+        i = i+ 1
+    end
+    return [arrEmails,arrPN,arrName]
 end
