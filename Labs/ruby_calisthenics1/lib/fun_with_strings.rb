21b10c1c337ec15b3a39c764f467fe8a202f754d
Saisiddharth Kandimalla <skandim1@uncc.edu>
2020-02-10 22:27:01 +0000

    Lab 9: Fun With Words - count words

--- Labs/ruby_calisthenics1/lib/fun_with_strings.rb
+++ Labs/ruby_calisthenics1/lib/fun_with_strings.rb
@@ -15,7 +15,17 @@ module FunWithStrings
     
   end
   def count_words
-    
+    x = 0
+    dictionary = {}
+    word = self.downcase
+    word = word.gsub(/[^a-zA-Z ]/, '')
+    word = word.split()
+    puts word
+    while x < word.length do
+      dictionary[word[x]] = dictionary[word[x]].to_i + 1
+      x = x + 1
+    end
+    return dictionary
     
   end
   def anagram_groups
