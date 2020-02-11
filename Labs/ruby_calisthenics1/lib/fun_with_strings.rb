81361e47beae2271e661a26e5aed14ab3f8a2caa
Saisiddharth Kandimalla <skandim1@uncc.edu>
2020-02-11 04:27:26 +0000

    Lab 9: Fun with Words - anagram_groups

--- Labs/ruby_calisthenics1/lib/fun_with_strings.rb
+++ Labs/ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,11 +1,11 @@
 module FunWithStrings
   def palindrome?
     x = 0
-    word = self.downcase
-    word = word.gsub(/[^a-zA-Z]/, '')
-    while x < word.length/2 do
+    phrase = self.downcase
+    phrase = phrase.gsub(/[^a-zA-Z]/, '')
+    while x < phrase.length/2 do
       
-      if word[x] != word[word.length-x-1]
+      if phrase[x] !=phrase[phrase.length-x-1]
         return false
       end
       x = x + 1
@@ -14,24 +14,36 @@ module FunWithStrings
     return true
     
   end
+  
   def count_words
     x = 0
     dictionary = {}
-    word = self.downcase
-    word = word.gsub(/[^a-zA-Z ]/, '')
-    word = word.split()
-    puts word
-    while x < word.length do
-      dictionary[word[x]] = dictionary[word[x]].to_i + 1
+    phrase = self.downcase
+    phrase = phrase.gsub(/[^a-zA-Z ]/, '')
+    phrase = phrase.split()
+    while x < phrase.length do
+      dictionary[phrase[x]] = dictionary[phrase[x]].to_i + 1
       x = x + 1
     end
     return dictionary
     
   end
   def anagram_groups
-    # your code here
-  end
-end
+    x = 0
+    dictionary = Hash.new{|hsh,key| hsh[key] = [] }
+    phrase = self.split()
+    
+    while x < phrase.length do 
+      temp = phrase[x].downcase.chars.sort
+      
+      dictionary[temp].push phrase[x]
+      x = x + 1
+    end
+    puts dictionary
+    return dictionary.values
+  end 
+end 
+
 
 # make all the above functions available as instance methods on Strings:
 
