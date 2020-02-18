b78530bc8bb77a9c5252a1b9ca12a362e265368c
Saisiddharth Kandimalla <skandim1@uncc.edu>
2020-02-18 06:34:03 +0000

    Ruby OO Basics – Part 1

--- Labs/RubyBasics3/lib/rubybasics3.rb
+++ Labs/RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,10 @@
 # Ruby Basics Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    attr_accessor :isbn
+    attr_accessor :price
+    def initialize(isbn, price)
+        @isbn = isbn 
+        @price = price
+    end
 end
