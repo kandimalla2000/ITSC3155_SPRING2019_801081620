ee877f505d812bc0dbd54e178f2fc783390d1e17
Saisiddharth Kandimalla <skandim1@uncc.edu>
2020-03-28 03:02:19 +0000

    Add blogger adds an article scenario 2

--- Labs/blog/app/controllers/articles_controller.rb
+++ Labs/blog/app/controllers/articles_controller.rb
@@ -1,19 +1,32 @@
 class ArticlesController < ApplicationController
-    def new
-        @article = Article.new
-    end 
-    
     def index
         @articles = Article.all
-    end 
-    def show 
+    end
+    
+    def show
         @article = Article.find(params[:id])
-    end 
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+    
     def edit
         @article = Article.find(params[:id])
     end
+    
     def update
         @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             redirect_to @article
         else
@@ -24,25 +37,12 @@ class ArticlesController < ApplicationController
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
-        redirect_to articles_path
-    end
-    
-    def create
-        @article = Article.new(article_params)
-        byebug
         
-        if @article.save
-            byebug
-            redirect_to @article
-        else
-            render 'new'
-        end
-        
+        redirect_to articles_path 
     end
 end
 
 private
     def article_params
         params.require(:article).permit(:title, :text)
-    
-    end 
+    end
